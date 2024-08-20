ae7f77947a6b8b7b55266f43ac933203d030d7ad
Network <89808327+ne1work@users.noreply.github.com>
2024-08-21 00:17:31 +0200

    Update KinogerPlugin.kt

--- Kinoger/src/main/kotlin/com/ne1work/KinogerPlugin.kt
+++ Kinoger/src/main/kotlin/com/ne1work/KinogerPlugin.kt
@@ -9,6 +9,5 @@ class KinogerPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Kinoger())
-        registerExtractorAPI(Kinogeru())
     }
 }
